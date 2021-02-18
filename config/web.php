f9df596575e63a3ffccd9ead1bb5212bfe9e5c88
LukichevSergey <Lukichev_s@mail.ru>
2021-02-18 22:32:49 +0700

    Реализация пагинации. Постраничного вывода товаров

--- config/web.php
+++ config/web.php
@@ -53,8 +53,9 @@ $config = [
             'showScriptName' => false,
             'enableStrictParsing' => false,
             'rules' => [
+                'category/<id:\d+>/page/<page:\d+>' => 'category/view',
                 'category/<id:\d+>' => 'category/view',
-                'product/<id:\d+>' => 'product/view'
+                'product/<id:\d+>' => 'product/view',
             ],
         ],
 
